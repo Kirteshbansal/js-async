72912829b161b6b89cb97403a2125f5b6a6bec56
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-08-31 22:56:00 +0530

    Add promisified version of fetchRandomString()

--- random.js
+++ random.js
@@ -38,3 +38,24 @@ function fetchRandomNumbers() {
 
 fetchRandomNumbers().then(console.log);
 
+// Promise version of fetchRandomString
+function fetchRandomString() {
+    return new Promise((resolve, reject) => {
+      console.log("Fetching string...");
+      setTimeout(() => {
+        let result = "";
+        let characters =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let charactersLength = characters.length;
+        for (let i = 0; i < 5; i++) {
+          result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+          );
+        }
+        console.log("Received random string:", result);
+        resolve(result);
+      }, (Math.floor(Math.random() * 5) + 1) * 1000);
+    });
+  }
+  
+  fetchRandomString().then(console.log);
